88612adcac433a3f2260778090580307904f1a50
谷口和寛 <e1301628@gmail.com>
2017-12-03 14:04:51 +0900

    API fix
    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :users, only: [:show, :edit, :update]
   root 'home#index'
   get 'tweets' => 'tweets#index'
   get   'tweets/new'  =>  'tweets#new'
   post  'tweets' => 'tweets#create'
 
-  # devise_for :users,
-  # path: '',
-  # path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-  # controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+  devise_for :users,
+  path: '',
+  path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 end
